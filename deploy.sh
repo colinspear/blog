5615fc658c31bb9dcd9534b2150cf7405186071f
Colin Spear <colinspear@gmail.com>
2021-02-17 15:41:36 -0800

    update links

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#! /usr/bin/env zsh
 
 # If a command fails then the deploy stops
 set -e
@@ -6,7 +6,7 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t ezhil # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
